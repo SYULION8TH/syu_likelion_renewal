28d0c8792c35dcbefa61760aa068dbbfc8313076
LEEJUNGEUN <wwjddms2@nate.com>
2020-09-15 11:17:36 +0900

    fix model reference error

--- discussion/views.py
+++ discussion/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
-from likelion_syu_renewal.main import models
+from django.shortcuts import render 
+from django.http import JsonResponse
+from main import models
 
 # Create your views here.
+
+def home(request):
+   # return render(request, "./ ")
+   return JsonResponse({"key" : "Hello" })
--- timeline/views.py
+++ timeline/views.py
@@ -1,3 +1,3 @@
 from django.shortcuts import render
-from likelion_syu_renewal.main import models
+from main import models
 # Create your views here.
