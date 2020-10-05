7f9e3e22f5d932f41abc0981d606378187ab51f7
yun-n <jyk0321@likelion.org>
2020-10-05 18:25:22 +0900

    dis.models 삭제, dis.views 수정 #8

--- discussion/views.py
+++ discussion/views.py
@@ -1,16 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.core.paginator import Paginator 
-from .models import *
-# from .forms import CommentForm
+from main.models import *
 
-
-
-def base(request):
-   return render(request,'html/base.html')
 
 def blog(request):
-    post = MainDiscussion.objects.all()
+    post = discussion.objects.all()
     page = request.GET.get('page', '1')
     paginator = Paginator(post, 10)
     page_obj = paginator.get_page(page)
@@ -18,18 +13,18 @@ def blog(request):
     return render(request, 'html/blog.html', context)
 
 def posting(request, pk):
-    post = MainDiscussion.objects.get(pk=pk)
+    post = discussion.objects.get(pk=pk)
     return render(request, 'html/posting.html', {'post':post})
 
 def new_post(request):
     if request.method == 'POST':
         if request.POST['dis_title']:
-            new_article=MainDiscussion.objects.create(
+            new_article=discussion.objects.create(
                 dis_title=request.POST['dis_title'],
                 dis_desc=request.POST['dis_desc'],
             )
         else:
-            new_article=MainDiscussion.objects.create(
+            new_article=discussion.objects.create(
                 dis_title=request.POST.get['dis_title'],
                 dis_desc=request.POST.get['dis_desc'],                
             )
@@ -37,7 +32,7 @@ def new_post(request):
     return render(request, 'html/new_post.html')
 
 def remove_post(request, pk):
-    post = MainDiscussion.objects.get(pk=pk)
+    post = discussion.objects.get(pk=pk)
     if request.method == 'POST':
         post.delete()
         return redirect('/blog/')
